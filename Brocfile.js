3228a33a7e7fadcd7ac477d17d51a9acbcae6631
Sukjoon Kim <sjoonk@gmail.com>
2015-01-08 23:26:40 +0900

    update Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,4 @@
 // Brocfile.js
-// Reference: https://github.com/broccolijs/broccoli-sample-app/blob/master/Brocfile.js
 
 module.exports = (function() {
 
@@ -10,19 +9,13 @@ module.exports = (function() {
     // var findBowerTrees = require('broccoli-bower');
     var mergeTrees = require('broccoli-merge-trees');
 
-    // concat the JS
-    var scripts = concat('app/scripts', {
-        inputFiles: ['**/*.js'],
-        outputFile: '/assets/scripts.js'
-    });
+    // var _ = require('lodash');
+    // var templateBuilder = require('broccoli-template-builder');
+    // var wrapFiles = require('broccoli-wrap');
 
+    var jst = require('broccoli-jst');
 
     // concat dependencies
-    // var deps = pickFiles('bower_components', {
-    //     srcDir: '/',
-    //     destDir: '/deps'
-    // });
-
     var deps = concat('bower_components', {
         inputFiles: [
             'jquery/dist/jquery.js',
@@ -32,13 +25,50 @@ module.exports = (function() {
         outputFile: '/assets/deps.js'
     });
 
+    // build templates
+    // var templates = templateBuilder('app/scripts/templates', {
+    //     extensions: ['ejs'], // required
+    //     outputFile: 'assets/templates.js', // required
+    //     namespace: 'JST',      // optional (defaults to 'JST')
+    //     compile: function(string) {
+    //         return _.template(string).source;
+    //     }
+    // });
+    
+    // prepare scripts as tree
+    var scripts = 'app/scripts';
+    
+    // compile JST (apply filter)
+    scripts = jst(scripts, { extensions: ['ejs'] });
+    
+    // concat the JS
+    scripts = concat(scripts, {
+        inputFiles: [
+            'main.js',
+            'models/*.js',
+            'collections/*.js',
+            'templates/**/*.js',
+            'views/**/*.js',
+            'routes/*.js'
+        ],
+        outputFile: '/assets/scripts.js'
+    });
+
 
     // compile the SASS
     var styles = compileSass(['app/styles'], 'main.scss', 'assets/styles.css');
 
 
+    // extract extra static assets
+    var extras = pickFiles('app', {
+        srcDir: '/',
+        files: ['index.html'],
+        destDir: '/'
+    });
+
     // merge all the trees together
-    var tree = mergeTrees([deps, scripts, styles, 'app']);
+    // var tree = mergeTrees([deps, scripts, 'app']);
+    var tree = mergeTrees([deps, scripts, styles, extras]);
     return tree;
 
 })();
